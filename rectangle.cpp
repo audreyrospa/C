80199855f6fd0a1a02eda476ee3e41ef609c1c4c
Audrey Rosa Paramita <76872450+audreyrospa@users.noreply.github.com>
2021-09-24 00:13:30 +0700

    Basic rectangle
    This Program calculate the area of a rectangle

---
+++ rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int length;
+	cout << "\nlength:";
+	cin >>length;
+
+	float area=length*length;
+	cout << "\nThe area of the rectangle:" << area << endl;
+
+	cin.get();
+	return 0;
+}
+	
+
